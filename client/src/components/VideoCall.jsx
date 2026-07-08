afd883f8f2ae24565592b509ddc58db8850888b7
Garvit1908 <ronitsaini940@gmail.com>
2026-07-08 14:04:00 +0530

    Add STUN servers for video calling across networks

--- client/src/components/VideoCall.jsx
+++ client/src/components/VideoCall.jsx
@@ -34,6 +34,13 @@ export default function VideoCall({ socket, callData, currentUser, onEndCall }) 
           initiator: true,
           trickle: false,
           stream: mediaStream,
+          config: {
+            iceServers: [
+              { urls: "stun:stun.l.google.com:19302" },
+              { urls: "stun:stun1.l.google.com:19302" },
+              { urls: "stun:stun2.l.google.com:19302" },
+            ],
+          },
         });
 
         peer.on("signal", (signal) => {
@@ -71,6 +78,13 @@ export default function VideoCall({ socket, callData, currentUser, onEndCall }) 
           initiator: false,
           trickle: false,
           stream: mediaStream,
+          config: {
+            iceServers: [
+              { urls: "stun:stun.l.google.com:19302" },
+              { urls: "stun:stun1.l.google.com:19302" },
+              { urls: "stun:stun2.l.google.com:19302" },
+            ],
+          },
         });
 
         peer.on("signal", (signal) => {
